68fca11d0b1207f17c18d3ab3d570e0e44cd9be6
bezitok <nguyenhaikiller@gmail.com>
2019-06-03 01:54:30 +0700

    commit

--- SublimeTextDemo/index.js
+++ SublimeTextDemo/index.js
@@ -14,7 +14,7 @@ class SublimeTextDemo extends Component {
 	render(){
 		return(
 			<View style = {styles.maunen}>
-				<Text style = {styles.mauchu}></Text>
+				<Text style = {styles.mauchu}>Sau đây tôi xin test Github</Text>
 			</View>
 		);
 	}
